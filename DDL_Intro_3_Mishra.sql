65d3910cccc621c9e1e43c7a1e2b933a20bf9b41
Suraj Mishra <75450225+mishrasur7@users.noreply.github.com>
2021-10-03 22:27:52 +0300

    Add files via upload

---
+++ DDL_Intro_3_Mishra.sql
@@ -0,0 +1,11 @@
+INSERT INTO Department(deptno, deptname) VALUES
+(30, 'Management') 
+
+INSERT INTO Employee(empno, empname, deptno) VALUES
+(4, 'Athena', 30) 
+
+SELECT empname FROM Employee
+
+SELECT empname FROM Employee WHERE empno = 2
+
+SELECT empname, deptno FROM Employee WHERE deptno = 10
